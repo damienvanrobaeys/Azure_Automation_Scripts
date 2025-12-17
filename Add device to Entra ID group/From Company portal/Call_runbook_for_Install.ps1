6380c98a4365527bfbc9f77ae5c710790c14d405
Damien Van Robaeys <damien.vanrobaeys@gmail.com>
2025-12-17 21:44:48 +0100

    Update Call_runbook_for_Install.ps1

--- Add device to Entra ID group/From Company portal/Call_runbook_for_Install.ps1
+++ Add device to Entra ID group/From Company portal/Call_runbook_for_Install.ps1
@@ -3,7 +3,8 @@ DeviceName ="$env:COMPUTERNAME";
 Action ="Add"  
 }  
 
-$webhookURI = "https://e92cfc59-eda2-4623-b29e-29eeb48a53c2.webhook.we.azure-automation.net/webhooks?token=8byxdKjN0bAp%2bwixjInBjo2%2bUTpTUnpdy678HeJ69mM%3d"
+$webhookURI = ""
 
 $body = ConvertTo-Json -InputObject $params  
+
 $response = Invoke-WebRequest -Method Post -Uri $webhookURI -Body $body -UseBasicParsing  
